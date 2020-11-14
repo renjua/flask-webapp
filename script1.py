ca386fac0b384fd2e01f6f66dc704a58b3df7992
Renju Abraham <abraham.renju@gmail.com>
2020-11-14 12:57:04 +0530

    flask app first commit

---
+++ script1.py
@@ -0,0 +1,16 @@
+import flask
+import sys
+
+app=flask.Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"]=0
+
+@app.route("/")
+def home():
+  return flask.render_template("home.html")
+
+@app.route("/about/")
+def about():
+  return flask.render_template("about.html")
+
+if __name__=="__main__":
+  app.run(debug=True )
